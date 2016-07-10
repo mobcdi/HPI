ba292e5c4ee182b07f994aa98c0580e473181663
mobcdi <mobcdi@users.noreply.github.com>
2016-07-10 12:32:30 +0100

    removed python install

--- OpenCVGCE.sh
+++ OpenCVGCE.sh
@@ -32,7 +32,6 @@ echo "Installing Packages to handle images"
 sudo apt-get -y install libtiff4-dev libjpeg8-dev libjasper-dev libpng12-dev
 echo "Installing Packages to handle video streams"
 sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev
-sudo apt-get -y install python-dev python-numpy
 sudo apt-get -y install libtbb-dev libeigen3-dev
 sudo apt-get -y install libqt4-dev libgtk2.0-dev
 sudo apt-get -y install libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev
